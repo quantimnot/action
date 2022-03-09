d5b3f3b98a66cd7c144cad9fe314a66307bbfebb
Jacob Carlborg <doob@me.com>
2022-03-09 08:53:06 +0100

    fixup! Test FreeBSD on Linux

--- src/qemu_vm.ts
+++ src/qemu_vm.ts
@@ -27,10 +27,9 @@ export abstract class Vm extends vm.Vm {
       '-smp', `cpus=${this.configuration.cpuCount},sockets=${this.configuration.cpuCount}`,
       '-m', this.configuration.memory,
       
-      '-device', 'virtio-scsi-pci',
-      '-device', 'scsi-hd,drive=drive0,bootindex=0',
+      '-device', 'virtio-blk-pci,drive=drive0,bootindex=0',
       '-drive', `if=none,file=${this.configuration.diskImage},id=drive0,cache=writeback,discard=ignore,format=raw`,
-      '-device', 'scsi-hd,drive=drive1,bootindex=1',
+      '-device', 'virtio-blk-pci,drive=drive1,bootindex=1',
       '-drive', `if=none,file=${this.configuration.resourcesDiskImage},id=drive1,cache=writeback,discard=ignore,format=raw`,
 
       '-device', 'virtio-net,netdev=user.0',
